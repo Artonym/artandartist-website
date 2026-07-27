3a443935512dc8f7b2133241d329a08c4d5cf3d4
Akshay Varma <akshayvarma0722@gmail.com>
2026-07-27 21:50:53 +0530

    Addedprivacy policy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, useLocation, useNavigate } from "react-router-dom"
+import { HashRouter as Router, Routes, Route, useLocation, useNavigate } from "react-router-dom"
 
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
